be6afecb9cbde5dd556f51721d982253519052d7
adradan <ericvsolcruz@gmail.com>
2020-10-13 23:40:15 -0400

    Fixed search results appearing/disappearing when clicking away or onto input.

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -5,13 +5,12 @@ function addInput() {
     let lastIndex = inputArray.length - 1;
     let lastInput = inputArray[lastIndex];
     let latestIdNum = Number(lastInput.id.split('-')[1]);
-    // let lineBreak = document.createElement('br');
-    // form.appendChild(lineBreak);
     newInput.type = 'text';
     newInput.id = `artist-${latestIdNum + 1}`;
     newInput.name = `artist-${latestIdNum + 1}`;
     newInput.classList.add('artist-input');
     newInput.required = true;
+    newInput.setAttribute('autocomplete', 'off');
     newDiv.id = `artist-div-${latestIdNum + 1}`;
     newDiv.classList.add('input-div');
 
@@ -21,7 +20,6 @@ function addInput() {
         let br = document.createElement('br');
         let div = form.appendChild(newDiv);
         div.appendChild(newInput);
-        // div.appendChild(br);
     } else {
         newBtn.textContent = '+';
         newBtn.classList.add('btn-add-input');
@@ -48,15 +46,8 @@ function removeInput() {
             remainingInput[i].id = `artist-${prevIndex - 1}`;
             remainingInput[i].setAttribute('name', `artist-${prevIndex = 1}`);
             remainingInput[i].parentElement.setAttribute('id', `artist-div-${prevIndex - 1}`);
-        } else {
-            continue;
         }
-
     }
-    // if (deletedIndex === 1) {
-    //     let brs = form.querySelectorAll('br');
-    //     brs[0].remove();
-    // }
     this.remove();
 }
 
@@ -78,6 +69,7 @@ let form = document.querySelector('.form');
 buttonAdd.addEventListener('click', changeAttrCreate);
 
 function findResults(elem, resultsList) {
+    // Gets results from server and appends results to search results
     let search = $(elem).val();
     let currentDiv = $($(elem).parent())[0]
     let resultsDiv = $(currentDiv).find('.search-results');
@@ -90,12 +82,7 @@ function findResults(elem, resultsList) {
             $(resultsList).empty();
             if (data.length !== 0) {
                 for (let i = 0; i < len; i++) {
-                    // let style = 'style="position: absolute; top: 0; left: 0;"';
                     let name = data[i]['name'];
-                    // if (i === 0) {
-                    //     console.log(i);
-                    //     style = 'style="position: absolute;"'
-                    // }
                     resultsList.append(`<li class="result artist">` + name + '</li>');
                 }
             } else {
@@ -106,10 +93,10 @@ function findResults(elem, resultsList) {
 }
 
 function checkClick() {
-    $('input').attr('autocomplete', 'off');
     let currentBox;
     let resultsList;
     $(document).on('input', '.artist-input', function(e) {
+        // Creates a results menu under search box and finds results from spotify
         currentBox = e.target;
         let divElem = $($(currentBox).next()[0]).next();
         if (divElem[0]) {
@@ -123,81 +110,55 @@ function checkClick() {
         }
         findResults(currentBox, resultsList);
     });
+    $(document).on('click', 'li.result', function () {
+        // Autofilling Input to selected artist from menu
+        let text = this.textContent;
+        let inputDiv = this.parentElement.parentElement.parentElement;
+        let inputBox = inputDiv.getElementsByClassName('artist-input');
+        inputBox[0].value = text;
+    })
     $(document).click(function(e) {
-        let elem = e.target;
-        console.log(elem);
-        let searchResults;
-        console.log(prevTarget);
-        console.log(elem);
-        // FIX SEARCH RESULTS APPEARING/HIDING
-        if ($(elem).attr('class') === 'artist-input' && elem.id != prevTarget.id) {
-            console.log('haha');
-            prevTarget = elem;
-            searchResults = $($(elem).parent()).find('.search-results');
-            let parent = $(prevTarget).parent();
-            parent = $(parent).find('.search-results');
-            $(parent).hide();
-            // $($($(prevTarget).parent()).find('.search-results')).hide();
-            if (searchResults.length) {
-                $(searchResults).show();
+        // Handle clicking away from menu
+        let prevParent;
+        let prevResults;
+        let resultsDiv;
+        let currentElem = e.target;
+        let currentElemParent = currentElem.parentElement;
+        if (prevTarget === null) {
+            // NO PREVTARGET, SHOW MENU
+            prevTarget = currentElem;
+        } else if (currentElem.classList.contains('artist-input')) {
+            // CLICKED AN INPUT BOX, SHOW ITS MENU
+            resultsDiv = currentElemParent.getElementsByClassName('search-results');
+            if (resultsDiv.length) {
+                // IF THEY HAVE SEARCHED, SHOW MENU
+                resultsDiv[0].style.visibility = 'visible';
             }
-        } else if ($(elem).attr('class') === 'artist-input' || $(elem).attr('class') === 'result' || $(elem).attr('class') === 'empty'){
-            console.log('boohoo');
-            prevTarget = elem;
-            searchResults = $(elem).parent().find('.search-results');
-            if (searchResults.length) {
-                console.log('searh');
-                $(searchResults).show();
+            if (prevTarget.id === currentElem.id) {
+                // CLICKED ON THE SAME BOX, KEEP SHOWING MENU
+                // LEFT FOR REFERENCE
+            } else if (prevTarget.id !== currentElem.id && prevTarget.nodeName === currentElem.nodeName) {
+                // CLICKED ON A DIFFERENT BOX
+                prevParent = prevTarget.parentElement;
+                prevResults = prevParent.getElementsByClassName('search-results');
+                if (prevResults.length) {
+                    prevResults[0].style.visibility = 'hidden';
+                }
             }
         } else {
-            prevTarget = elem;
-            elem = $(elem).parent();
-            searchResults = $(elem).find('.search-results');
-            $(searchResults).hide();
+            // CLICKING ON IRRELEVANT ELEMENTS
+            prevParent = prevTarget.parentElement;
+            if (prevParent !== null) {
+                prevResults = prevParent.getElementsByClassName('search-results');
+                if (prevResults.length) {
+                    prevResults[0].style.visibility = 'hidden';
+                }
+            }
         }
+        prevTarget = currentElem;
     })
 }
 
-let prevTarget;
+let prevTarget = null;
 
 $(document).ready(checkClick)
-
-// $(document).ready(function() {
-//     $('.artist-input').on('input', function () {
-//         let search = $(this).val()
-//         if (search != '') {
-//             $('#search-result').show();
-//             $.getJSON($SCRIPT_ROOT + '/ajax/search_artist', {
-//             artist: search
-//             }, function(data) {
-//                 data = data['artists']
-//                 let len = data.length;
-//                 $('#search-result').empty();
-//                 if (data.length !== 0) {
-//                     for (let i = 0; i < len; i++) {
-//                         let name = data[i]['name'];
-//                         $('#search-result').append("<p class='result' value='" + name + "'>" + name + "</p>");
-//                     }
-//                     $('#search-result li .result').bind('click', function () {
-//                         console.log($(this).parent().parent());
-//                     })
-//                 } else {
-//                     $('#search-result').append("<p value='empty' class='empty'>No Results Found</p>");
-//                 }
-//             })
-//         } else {
-//             $('#search-result').empty();
-//         }
-//     });
-//     $(document).click(function(e) {
-//         let elem = e.target;
-//         if ($(elem).attr('class') === 'result' || $(elem).attr('class') === 'empty') {
-//             return false;
-//         } else {
-//             $('#search-result').hide();
-//         }
-//     })
-// })
-
-
-
