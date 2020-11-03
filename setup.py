7d9716969d0c24d9523fdec39d78d511c404fe4f
adradan <ericvsolcruz@gmail.com>
2020-11-02 22:49:40 -0500

    Deploying to Heroku

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flaskRandomizer',
+    version='1.0',
+    packages=['app', 'playlist_randomizer'],
+    url='',
+    license='',
+    author='eric-vsolcruz',
+    author_email='ericvsolcruz@gmail.com',
+    description='Creates a randomized Spotify playlist with your artists you choose.'
+)
