5cc2affb1cf43c0ec7aaed97ae6f618afa01c170
adradan <ericvsolcruz@gmail.com>
2020-11-05 22:50:33 -0500

    Attempting to fix session not persisting on Google Chrome.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 csrf = CSRFProtect(app)
 
 from app import routes
